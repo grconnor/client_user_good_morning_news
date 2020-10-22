9fe5f53f3a0b303d5f35856c5e3e1bcd26ecee64
FacundoOs <osores.facundo@gmail.com>
2020-10-22 21:16:31 +0200

    add sad path in test

--- cypress/integration/vistorCanRegisterAnAccount.feature.js
+++ cypress/integration/vistorCanRegisterAnAccount.feature.js
@@ -9,12 +9,53 @@ describe("Visitor can register an account", () => {
   });
 
   context("Unsuccessfully", () => {
-    it("user can click on register button and see registration form", () => {
-      cy.get('[data-cy="password-confirmation"]').type("wrongpassword");
-      cy.get('[data-cy="submit"]').contains("Submit").click();
-      cy.get('[data-cy="registration-message"]').contains(
-        "Your passwords are not matching. Please put in matching passwords."
-      );
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles",
+        response: "fixture:articles_index.json",
+      });
+      cy.visit("/");
+    });
+    it("Unsuccessfully with wrong password", () => {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/v1/aut**",
+        response: {
+          errors: ["Passwords do not mactch. Please try again."],
+        },
+        status: "422",
+      });
+      cy.get('[data-cy="sign-up-button"]').contains("Sign Up").click();
+
+      cy.get('[data-cy="sign-up-form"]').within(() => {
+        cy.get('[data-cy="name"]').type("Facundo Osores");
+        cy.get('[data-cy="email"]').type("registered@mail.com");
+        cy.get('[data-cy="password"]').type("password");
+        cy.get('[data-cy="password-confirmation"]').type("wrongPassword");
+        cy.get('[data-cy="submit"]').contains("Submit").click();
+      });
+    });
+    
+    it("Unsuccessfully - email already exist", () => {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/v1/aut**",
+        response: {
+          errors: ["The mail already exists. Please choose another"],
+        },
+        status: "422",
+      });
+      cy.get('[data-cy="sign-up-button"]').contains("Sign Up").click();
+
+      cy.get('[data-cy="sign-up-form"]').within(() => {
+        cy.get('[data-cy="name"]').type("Facundo Osores");
+        cy.get('[data-cy="email"]').type("registered@mail.com");
+        cy.get('[data-cy="password"]').type("password");
+        cy.get('[data-cy="password-confirmation"]').type("password");
+        cy.get('[data-cy="submit"]').contains("Submit").click();
+      });
     });
   });
 });
