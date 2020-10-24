a965bbed5cd4f3527feab93e613d50ac685210dd
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-24 17:17:39 +0200

    Test WIP

--- cypress/integration/userCanReadBasedOnLocation.feature.js
+++ cypress/integration/userCanReadBasedOnLocation.feature.js
@@ -1,14 +1,31 @@
 const stubLocation = require("../support/stubLocation");
 
+const apiKey = process.env.REACT_APP_OPEN_CAGE_API_KEY;
+
+
 describe("user can read articles ", () => {
   context('when in sweden', () => {
     before(() => {
       cy.server()
+      // cy.route({
+      //   method: "GET",
+      //   url: "http://localhost:3000/api/v1/articles?latitude=60&longitude=18",
+      //   response: "fixture:sweden_articles.json"
+      // })
+      
       cy.route({
         method: "GET",
-        url: "http://localhost:3000/api/v1/articles?latitude=60&longitude=18",
-        response: "fixture:sweden_articles.json"
-      })
+        url: `http://api.opencagedata.com/geocode/v1/json?q=60%2C18&language=en&key=${apiKey}`,
+        response: "fixture:sweden_index.json",
+      });
+
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles/?location=sweden",
+        response: "fixture:sweden_index.json",
+      });
+
+
       cy.visit("/", stubLocation({
         latitude: 60, 
         longitude: 18,
