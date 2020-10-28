94c5c728540a24ac202eb6db2c4168368b59e232
emiliano-ma <emilianomainero@gmail.com>
2020-10-28 23:13:11 +0100

    Style for Articles List

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 const ArticlesCard = ({ article }) => {
   return (
-    <Card>
-      <Card.Content as={Link} to={`/articles/${article.id}`}>
+    <Card as={Link} to={`/articles/${article.id}`}>
+      <Card.Content >
         <Card.Header data-cy="title">{article.title}</Card.Header>
-        {article.image && <Image data-cy="image" src={article.image} />}
         <Card.Description data-cy="teaser">{article.teaser}</Card.Description>
       </Card.Content>
+      {article.image && <Image data-cy="image" src={article.image} />}
     </Card>
   );
 };
